4184219c86b598f521ecbe8dcc55102a6080d437
AleksandraMolo <ola.molo@onet.pl>
2023-11-09 11:48:00 +0100

    Poprawione zadanie DZIEŃ 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,17 @@
 ﻿
 var name = "Ewa";
-var gender = true;
+var isWoman = true;
 var age = 29;
 
-if (age == 33 && gender == true)
+if (name == "Ewa" && age == 33 && isWoman == true)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else if (age<30 && gender==true)
+else if (isWoman == true && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (gender==false && age <18)
+else if (isWoman == false && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
