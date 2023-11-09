c4b711694789a3a4fb559f8c7461a8ed11335f7b
AleksandraMolo <ola.molo@onet.pl>
2023-11-09 10:41:18 +0100

    Zadanie DZIEŃ 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Ewa";
+var gender = true;
+var age = 29;
+
+if (age == 33 && gender == true)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age<30 && gender==true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (gender==false && age <18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
