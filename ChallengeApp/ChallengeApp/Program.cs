3d593bb10ea62e547fd04b60b10509538d96941d
AleksandraMolo <ola.molo@onet.pl>
2023-12-12 21:13:16 +0100

    Zadanie DZIEŃ 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,70 +1,48 @@
-﻿int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using ChallengeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employer1 = new Employee ("Asia", "Nowak",33);
+Employee employer2 = new Employee ("Kasia", "Kowalska",45);
+Employee employer3 = new Employee ("Andrzej", "Zapart",25);
 
-foreach (char letter in letters)
+
+
+employer1.AddScore(5);
+employer1.AddScore(7);
+employer1.AddScore(9);
+employer1.AddScore(2);
+employer1.AddScore(8);
+
+employer2.AddScore(6);
+employer2.AddScore(5);
+employer2.AddScore(3);
+employer2.AddScore(9);
+employer2.AddScore(8);
+
+employer3.AddScore(7);
+employer3.AddScore(6);
+employer3.AddScore(9);
+employer3.AddScore(8);
+employer3.AddScore(7);
+
+List<Employee> employers = new List<Employee>()
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    employer1,
+    employer2,
+    employer3
+};
 
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employer in employers)
+{
+    if (employer.Result > maxResult)
+    {
+        employeeWithMaxResult = employer;
+    }
 }
 
-Console.WriteLine("0 => " + counter0);
-Console.WriteLine("1 => " + counter1);
-Console.WriteLine("2 => " + counter2);
-Console.WriteLine("3 => " + counter3);
-Console.WriteLine("4 => " + counter4);
-Console.WriteLine("5 => " + counter5);
-Console.WriteLine("6 => " + counter6);
-Console.WriteLine("7 => " + counter7);
-Console.WriteLine("8 => " + counter8);
-Console.WriteLine("9 => " + counter9);
+var Result = employeeWithMaxResult;
+Console.WriteLine("Pracownik z najwyższą liczbą to " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " w wieku " + employeeWithMaxResult.Age + "  lat");
+
+
